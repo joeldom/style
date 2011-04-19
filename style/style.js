4bb2468457f51a121a89fffbc6128d9accdfecf9
Sven Hanssen <sven@luyfel.nl>
2011-04-19 12:53:36 +0200

    Just standard loading of .js files

--- style/style.js
+++ style/style.js
@@ -1,21 +1,9 @@
 head.js(
-  // System
   {modernizr: "style/system/modernizr.min.js"},
-  {iepngfix_tilebg: "style/system/compatibility/iepngfix/iepngfix_tilebg.js"},
-  // Plugin
-  {metadata: "style/plugin/metadata/metadata.js"},
-  {button: "style/plugin/button/button.js"},
-  {scrollto: "style/plugin/scrollto/scrollto.js"},
-  {colorbox: "style/plugin/colorbox/colorbox.js"},
-  {bettergrow: "style/plugin/bettergrow/bettergrow.js"}
+  {button: "style/plugin/button/button.js"}
 );
 
 head.ready("button", function()
 {
   SimplyButtons.init();
-});
-
-head.ready("bettergrow", function()
-{
-  $("textarea.grow").BetterGrow();
 });
